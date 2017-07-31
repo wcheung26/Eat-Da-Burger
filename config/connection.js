34492afe7e863c4e7c25123483bff08b5afd19b4
wcheung26 <william.cheung94@gmail.com>
2017-07-30 17:50:05 -0700

    fixing missing bracket in connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,12 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+  })
+};
 
 connection.connect(function(err) {
   if (err) {
